7dddc1ea6c83a410c39facc3fe2db727cac4ee92
PoChihChen <z834159672@gmail.com>
2015-05-11 14:54:50 +0800

    - Add: infrared, relay

---
+++ tessel/ir.js
@@ -0,0 +1,45 @@
+var tessel = require('tessel');
+var infraredlib = require('ir-attx4');
+var infrared = infraredlib.use(tessel.port['A']);
+var relaylib = require('relay-mono');
+var relay = relaylib.use(tessel.port['B']);
+
+// When we're connected
+infrared.on('ready', function() {
+    if (!err) {
+        console.log("Connected to IR!");
+    } else {
+        console.log(err);
+    }
+});
+
+// If we get data, print it out
+infrared.on('data', function(data) {
+    console.log("Received RX Data: ", JSON.stringify(data));
+    if (data.length > 10) {
+        console.log('Received signal');
+        console.log('Toggling relays...');
+        relay.toggle(1, function toggleOneResult(err) {
+            if (err) console.log("Err toggling 1", err);
+        });
+    }
+});
+
+relay.on('ready', function relayReady () {
+    console.log('Relay ready!');
+});
+
+// When a relay channel is set, it emits the 'latch' event
+relay.on('latch', function(channel, value) {
+    console.log('latch on relay channel ' + channel + ' switched to', value);
+});
+
+var arraysEqual = function(a, b) {
+    if (a.length != b.length)
+        return false;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i])
+            return false;
+    }
+    return true;
+};
---
+++ tessel/relay.js
@@ -0,0 +1,23 @@
+/**
+ * Created by chen on 2015/5/11.
+ */
+var tessel = require('tessel');
+var relaylib = require('relay-mono');
+
+var relay = relaylib.use(tessel.port['B']);
+
+// Wait for the module to connect
+relay.on('ready', function relayReady () {
+    console.log('Ready! Toggling relays...');
+    setInterval(function toggle() {
+        // Toggle relay channel 1
+        relay.toggle(1, function toggleOneResult(err) {
+            if (err) console.log("Err toggling 1", err);
+        });
+    }, 2000); // Every 2 seconds (2000ms)
+});
+
+// When a relay channel is set, it emits the 'latch' event
+relay.on('latch', function(channel, value) {
+    console.log('latch on relay channel ' + channel + ' switched to', value);
+});
