918031cfa24c8a5bca40134d39b70383ca83e9e5
Derek Nien <aegis60918@gmail.com>
2015-05-15 11:37:55 +0800

    - Add: camera exports functions

--- tessel/camera.js
+++ tessel/camera.js
@@ -11,30 +11,45 @@
  *********************************************/
 
 var tessel = require('tessel');
-var camera = require('camera-vc0706').use(tessel.port['A']);
+var camera = require('camera-vc0706').use(tessel.port['D']);
 
 var notificationLED = tessel.led[3]; // Set up an LED to notify when we're taking a picture
 
-// Wait for the camera module to say it's ready
-camera.on('ready', function() {
-    notificationLED.high();
-    // Take the picture
-    camera.takePicture(function(err, image) {
+var pictures = [];
+
+exports.takePicture = function() {
+    console.log('Taking a picture');
+    camera.takePicture(function (err, image) {
         if (err) {
             console.log('error taking image', err);
         } else {
             notificationLED.low();
             // Name the image
-            var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
+            //var name = 'picture-' + Math.floor(Date.now() * 1000) + '.jpg';
             // Save the image
-            console.log('Picture saving as', name, '...');
-            console.log(image);
-            process.sendfile(name, image);
+            //console.log('Picture saving as', name, '...');
+            //console.log(image);
+            //process.sendfile(name, image);
+            pictures.push(image);
             console.log('done.');
             // Turn the camera off to end the script
-            camera.disable();
+            //camera.disable();
         }
     });
+};
+
+exports.getPictures = function() {
+    return pictures;
+};
+
+exports.clearPictures = function() {
+    pictures.length = 0;
+};
+
+// Wait for the camera module to say it's ready
+camera.on('ready', function() {
+    notificationLED.high();
+    // Take the picture
 });
 
 camera.on('error', function(err) {
