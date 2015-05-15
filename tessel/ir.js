10ae4abdb7bf6141f6aa33eddcc9758754232221
PoChihChen <z834159672@gmail.com>
2015-05-15 11:08:37 +0800

    - Add: infrared controls servo and relay

--- tessel/ir.js
+++ tessel/ir.js
@@ -2,6 +2,10 @@ var tessel = require('tessel');
 var infraredlib = require('ir-attx4');
 var infrared = infraredlib.use(tessel.port['A']);
 var parseIR = require('./parseIR');
+var relayToggle = require('./relay').relayToggle;
+var servoLeft = require('./servo').servoLeft;
+var servoRight = require('./servo').servoRight;
+var autoPanorama = require('./servo').autoPanorama;
 
 // When we're connected
 infrared.on('ready', function() {
@@ -17,6 +21,27 @@ infrared.on('data', function(data) {
     console.log("Received RX Data: ", JSON.stringify(data));
     if (data.length > 10) {
         console.log('Received signal');
-        console.log(parseIR(data));
+        var button = parseIR(data);
+        //console.log(button);
+        switch (button) {
+            case 'leftButton':
+                servoLeft();
+                break;
+            case 'rightButton':
+                servoRight();
+                break;
+            case 'upButton':
+                break;
+            case 'powerButton':
+                relayToggle();
+                break;
+            case 'okButton':
+                break;
+            case 'playButton':
+                autoPanorama();
+                break;
+            default:
+                console.log('Unknown button');
+        }
     }
 });
--- tessel/servo.js
+++ tessel/servo.js
@@ -1,43 +1,51 @@
 /**
  * Created by derek on 2015/5/11.
  */
-
-/*********************************************
- This servo module demo turns the servo around
- 1/10 of its full rotation  every 500ms, then
- resets it after 10 turns, reading out position
- to the console at each movement.
- *********************************************/
-
 var tessel = require('tessel');
 var servolib = require('servo-pca9685');
-
 var servo = servolib.use(tessel.port['C']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
+var position = 0; // Target position of the servo between 0 (min) and 1 (max).
 
 servo.on('ready', function () {
-    var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
-    var step = 0.1;
-    servo.move(servo1, position);
-
     //  Set the minimum and maximum duty cycle for servo 1.
     //  If the servo doesn't move to its full extent or stalls out
     //  and gets hot, try tuning these values (0.05 and 0.12).
     //  Moving them towards each other = less movement range
     //  Moving them apart = more range, more likely to stall and burn out
     servo.configure(servo1, 0.05, 0.12, function () {
-        setInterval(function () {
-            console.log('Position (in range 0-1):', position);
-            //  Set servo #1 to position pos.
-            servo.move(servo1, position);
-
-            // Increment by 10% (~18 deg for a normal servo)
-            position += step;
-            if (position >= 1 || position <= 0) {
-                //position = 0; // Reset servo position
-                step = -step;
-            }
-        }, 2000); // Every 500 milliseconds
+        servo.move(servo1, position);
+        console.log('Servo is ready!');
     });
 });
+
+exports.servoLeft = function() {
+    position -= 0.1;
+    if (position < 0) {
+        position = 0;
+    }
+    servo.move(servo1, position);
+};
+
+exports.servoRight = function() {
+    position += 0.1;
+    if (position > 1) {
+        position = 1;
+    }
+    servo.move(servo1, position);
+};
+
+exports.autoPanorama = function() {
+    position = 0;
+    for (var i = 0; i < 10; i++) {
+        setInterval(function () {
+            //  Set servo #1 to position pos.
+            servo.move(servo1, position);
+            /////////////////////////////////// require camera to take a picture
+
+            // Increment by 10% (~18 deg for a normal servo)
+            position += 0.1;
+        }, 1000 * i); // Every 1 sec
+    }
+};
