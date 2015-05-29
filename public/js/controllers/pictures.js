8e241a5219315bcb0a190edb59d894b5b6768a32
Derek Nien <aegis60918@gmail.com>
2015-05-29 22:23:40 +0800

    - Add: pictures client-side and server-side

--- public/js/controllers/pictures.js
+++ public/js/controllers/pictures.js
@@ -4,10 +4,33 @@
 
 angular.module('app')
     .controller('picturesController', function ($scope, $http) {
-        $scope.pictureUrls = [];
+        $scope.slideInterval = 5000;
+
+        $scope.allPictures = {
+            single: [],
+            series: []
+        };
+
+        $scope.activePictureSeries = 0;
+
+        $scope.nextPictureSeries = function() {
+            if($scope.activePictureSeries < $scope.allPictures.series.length - 1) {
+                $scope.activePictureSeries++;
+            } else {
+                $scope.activePictureSeries = 0;
+            }
+        };
+
+        $scope.prevPictureSeries = function() {
+            if($scope.activePictureSeries > 0) {
+                $scope.activePictureSeries--;
+            } else {
+                $scope.activePictureSeries = $scope.allPictures.series.length - 1;
+            }
+        };
 
         $http.get('api/tessel/pictures/')
             .success(function (data, status, headers, config) {
-                $scope.pictureUrls = data;
+                $scope.allPictures = data;
             });
     });
